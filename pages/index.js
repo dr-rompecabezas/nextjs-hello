d19d5739b7ef8f7ea534ce01e9c048721ee84d29
Felipe Villegas <felavid@gmail.com>
2021-11-24 02:37:57 -0500

    Removed leftover code from experimenting with Strapi

--- pages/index.js
+++ pages/index.js
@@ -31,30 +31,3 @@ export const getStaticProps = async () => {
 //   }
 // }
 
-// import axios from 'axios';
-
-// const Home = ({ restaurants, error }) => {
-//   console.log(restaurants);
-//   if (error) {
-//     return <div>An error occured: {error.message}</div>;
-//   }
-//   return (
-//     <ul>
-//       {restaurants.map(restaurant => (
-//         <li key={restaurant.id}>{restaurant.name}</li>
-//       ))}
-//     </ul>
-//   );
-// };
-
-// Home.getInitialProps = async ctx => {
-//   try {
-//     const res = await axios.get('http://localhost:1337/categories');
-//     const restaurants = res.data;
-//     return { restaurants };
-//   } catch (error) {
-//     return { error };
-//   }
-// };
-
-// export default Home;
